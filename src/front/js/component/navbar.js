f75c74391eff1436f10b1811f19cb7919039465a
Pedro <pedro@MacBook-Pro-de-Pedro.local>
2024-04-21 16:57:06 -0400

    commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,10 +1,7 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
+import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	const { store, actions } = useContext(Context);
-
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
@@ -12,14 +9,9 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
 				<div className="ml-auto">
-					{
-						!store.token ?
-							<Link to="/login">
-								<button className="btn btn-primary">Log in</button>
-							</Link>
-							: <button className="btn btn-primary" onClick={() => actions.logout()}>Log out</button>
-					}
-
+					<Link to="/demo">
+						<button className="btn btn-primary">Check the Context in action</button>
+					</Link>
 				</div>
 			</div>
 		</nav>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,76 +1,52 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			token: null,
-			pets: [],
-
+			message: null,
+			demo: [
+				{
+					title: "FIRST",
+					background: "white",
+					initial: "white"
+				},
+				{
+					title: "SECOND",
+					background: "white",
+					initial: "white"
+				}
+			]
 		},
 		actions: {
-			syncTokenFromLocalStorage: () => {
-				const token = localStorage.getItem("token")
-				console.log("Application loaded, synching the local storage")
-				if (token && token !== "" && token !== undefined) setStore({ token: token })
+			// Use getActions to call a function within a fuction
+			exampleFunction: () => {
+				getActions().changeColor(0, "green");
 			},
 
-			logout: () => {
-				localStorage.removeItem("token")
-				console.log("Login out")
-				setStore({ token: null })
-			},
-			login: async (email, password) => {
-				const opts = {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify({
-						"email": email,
-						"password": password
-					})
-				};
-				try {
-					const resp = await fetch(process.env.BACKEND_URL + "api/token", opts)
-					if (resp.status !== 200) {
-						alert("There has been some error");
-						return false
-					}
-
+			getMessage: async () => {
+				try{
+					// fetching data from the backend
+					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
-					console.log("This came from the backend", data)
-					localStorage.setItem("token", data.access_token)
-					setStore({ token: data.access_token })
-					return true
-				}
-				catch (error) {
-					console.log("There was an error!!!", error)
-				}
-			},
-			getAuthorization: () => {
-				const opts = {
-					headers: {
-						"Authorization": "Bearer " + getStore().token
-					}
-				}
-				try {
-					const response = fetch(process.env.BACKEND_URL + "api/private", opts)
-					const data = response.json()
-					console.log("This authorization came from the backend", data)
 					setStore({ message: data.message })
-					return true
-				}
-				catch (error) {
-					console.log("There was an error!!!", error)
+					// don't forget to return something, that is how the async resolves
+					return data;
+				}catch(error){
+					console.log("Error loading message from backend", error)
 				}
 			},
-			getAllPets: async () => {
-				const response = await fetch(process.env.BACKEND_URL + "api/pets")
-				const data = await response.json()
-				setStore({ pets: data.pets })
-				// here we put 'data.pets' because that's what we wrote in the api.route for get_all_pets
-				//when we do a fetch, and the status is not ok, the first thing is to check the URL and check the response with insomnia!!!
-			},
+			changeColor: (index, color) => {
+				//get the store
+				const store = getStore();
 
+				//we have to loop the entire demo array to look for the respective index
+				//and change its color
+				const demo = store.demo.map((elm, i) => {
+					if (i === index) elm.background = color;
+					return elm;
+				});
 
+				//reset the global store
+				setStore({ demo: demo });
+			}
 		}
 	};
 };
